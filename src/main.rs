992963022d02823ea3d31cc84502aabeca95117e
Keith R Languet <krlanguet@gmail.com>
2019-01-07 21:49:13 -0600

    Completed first exercise!

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,90 @@
 // First learning Project - Integer Vector Operations
 
-fn get_sorted_ints() -> Vec<u32> {
-    use std::io::stdin;
+use std::collections::HashMap;
 
-    'outer: loop {
-        let mut input = String::new();
-        let mut input_nums: Vec<u32> = vec![];
+struct IntData {
+    ints: Vec<u32>,
+    sum: u32,
+    counts: HashMap<u32, u32>,
+}
 
-        println!("Please Enter a sequence of non-negative integers");
-        println!("\te.g. \"0 24 3 7\":");
-    
-        stdin().read_line(&mut input)
-            .expect("Sorry, Failed to read line");
+impl IntData {
+    fn get() -> IntData {
+        use std::io::stdin;
 
-        let input: Vec<&str> = input.trim()
-            .split_whitespace().collect();
+        'input: loop {
+            println!("Please Enter a sequence of non-negative integers");
+            println!("\te.g. \"0 24 3 7\":");
+        
+            let mut _str = String::new();
 
-        for x in &input {
-            match x.parse::<u32>() {
-                Ok(i) => input_nums.push(i),
-                _ => {
-                        println!("Sorry, try again.");
-                        continue 'outer;
+            stdin().read_line(&mut _str)
+                .expect("Sorry, Failed to read line");
+
+            let _strs: Vec<&str> = _str.trim()
+                .split_whitespace().collect();
+
+            if _strs.len() == 0 {
+                println!("Sorry, You didn't enter anything");
+                continue 'input;
+            }
+
+            let mut _ints: Vec<u32> = vec![];
+            let mut _sum: u32 = 0;
+            let mut _counts: HashMap<u32, u32> = HashMap::new();
+
+            for x in &_strs {
+                match x.parse::<u32>() {
+                    Ok(i) => {
+                        _ints.push(i);
+                        _sum += i;
+                        *_counts.entry(i).or_insert(0) += 1;
+                    },
+                    _ => {
+                        println!("Sorry, You entered {} which is not an integer.", x);
+                        continue 'input;
+                    },
                 }
             }
-        }
 
-        input_nums.sort();
-        return input_nums;
+            _ints.sort();
+
+            return IntData {
+                ints : _ints, 
+                sum : _sum,
+                counts : _counts,
+            }
+        }
+    }
+
+    fn mean(&self) -> f64 {
+        self.sum as f64 / (self.ints.len() as f64)
+    }
+
+    fn median(&self) -> f64 {
+        let len = self.ints.len();
+        match len % 2 {
+            0 => (self.ints[ len/2 ] as f64 + self.ints[ (len/2) - 1] as f64) / 2.0,
+            _ => self.ints[ len/2 ] as f64,
+        }
+    }
+
+    fn mode(&self) -> u32 {
+        self.counts
+            .iter()
+            .max_by_key(|&(_, value)| value)
+            .map(|(key, _)| *key)
+            .unwrap()
     }
 }
 
+
 fn main() {
-    let nums = get_sorted_ints();
-
-
-    println!("You entered {:?}", nums);
+    let data = IntData::get();
+    
+    //println!("You entered {:?}", data.ints);
+    
+    println!("Mean: {}", data.mean());
+    println!("Median: {}", data.median());
+    println!("Mode: {}", data.mode());
 }
